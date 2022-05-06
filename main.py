b17fc8081920affcdc5fa2dff542a8352228850f
zzzzz1st <zzzzz1st@yahoo.com>
2022-05-06 08:46:55 +0200

    Removed unused strings in the output file

--- main.py
+++ main.py
@@ -5,60 +5,55 @@ import acronymDic
 
 # helper function
 def edit_entity(e):
-    myStr = e.text.replace("\P", " ")
-    myStr = myStr.replace("\p", " ")
-    myStr = myStr.replace("xqc;", "")
-    myStr = myStr.replace("\A1;", "")
-    if "RISERVA" not in myStr:
-        lines.append(myStr.replace("  ", "\n"))
-    myStr = myStr.replace(")", "")
-    myStr = myStr.replace("(", "")
-    myStr = myStr.replace("/", "")
-    myStr = myStr.replace(".", "")
-    myStr = myStr.replace("-", "")
-    myStr = myStr.replace("DI ", "")
-    myStr = myStr.replace("ATTUATORE ", "")
-    myStr = myStr.replace("SENSORE ", "")
-    myStr = myStr.replace("SONDA ", "")
+    my_str = e.text.replace("\P", " ")
+    my_str = my_str.replace("\p", " ")
+    my_str = my_str.replace("xqc;", "")
+    my_str = my_str.replace("\A1;", "")
+    my_str = my_str.replace(")", "")
+    my_str = my_str.replace("(", "")
+    my_str = my_str.replace("/", "")
+    my_str = my_str.replace(".", "")
+    my_str = my_str.replace("-", "")
+    my_str = my_str.replace("DI ", "")
+    my_str = my_str.replace("ATTUATORE ", "")
+    my_str = my_str.replace("SENSORE ", "")
+    my_str = my_str.replace("SONDA ", "")
     # Caso nome sonda vf20 e funzionamento
-    array = myStr.split("  ")
+    array = my_str.split("  ")
     if len(array) > 1:
         array.pop(0)
-    myStr = array[0].strip()
-    myStrArray = myStr.split(" ")
-    myStrArray = list(dict.fromkeys(myStrArray))
-    if myStrArray[0] != "RISERVA":
+    my_str = array[0].strip()
+    my_str_array = my_str.split(" ")
+    my_str_array = list(dict.fromkeys(my_str_array))
+    if my_str_array[0] != "RISERVA":
         # Caso Sonda e Temperatura
         # Toglie elemento mandata e lo mette in fine
         nquadro = ""
         vquadro = False
-        if myStrArray[-1][0:1] == "Q":
-            nquadro = myStrArray.pop(-1)
+        if my_str_array[-1][0:1] == "Q":
+            nquadro = my_str_array.pop(-1)
             vquadro = True
-        if myStrArray[0] == "TEMPERATURA" or myStrArray[0] == "SCATTATO" or myStrArray[0] == "LIVELLO":
-            myStrArray.append(myStrArray.pop(0))
-            myStrArray.append(myStrArray.pop(0))
+        if my_str_array[0] == "TEMPERATURA" or my_str_array[0] == "SCATTATO" or my_str_array[0] == "LIVELLO":
+            my_str_array.append(my_str_array.pop(0))
+            my_str_array.append(my_str_array.pop(0))
         else:
             # Caso normale che comando va in fine
-            myStrArray.append(myStrArray.pop(0))
+            my_str_array.append(my_str_array.pop(0))
         if vquadro is True:
-            myStrArray.insert(0, nquadro)
-        # TODO HANDLE ELSE -  QUANDO TUTTI IF SONO FALSI DEVE ANDARE IN ELSE
-        lines.append(str(myStrArray))
-        for i in range(len(myStrArray)):
-            if myStrArray[i][0:1] != "Q":
-                acron = acronymDic.Dict.get(myStrArray[i])
+            my_str_array.insert(0, nquadro)
+        for i in range(len(my_str_array)):
+            if my_str_array[i][0:1] != "Q":
+                acron = acronymDic.Dict.get(my_str_array[i])
                 if acron is None:
-                    acron = myStrArray[i].capitalize()
+                    acron = my_str_array[i].capitalize()
                     # Caso Sez passa if
                     if len(acron) > 3:
                         acron = acron[0:3]
                     # Caso Caldo C passa if
                 if len(acron) > 1:
                     acron = "_" + acron
-                myStrArray[i] = acron
-        lines.append("".join(myStrArray).lstrip("_"))
-        lines.append("---------------------------------------")
+                my_str_array[i] = acron
+        lines.append("".join(my_str_array).lstrip("_"))
 
 
 fileName = "BS16Y3"
